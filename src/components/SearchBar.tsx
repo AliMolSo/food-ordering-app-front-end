610382f625b827dd1bf8eb33cc39f74d8415e75c
Ali Ms <ms8135566@gmail.com>
2024-08-04 17:46:05 +0330

    handle reset

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,7 +22,7 @@ type Props = {
   searchQuery?: string;
 };
 
-const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
+const SearchBar = ({ onSubmit, placeHolder, searchQuery }: Props) => {
   const form = useForm<SearchForm>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -30,10 +30,7 @@ const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
     },
   });
 
-  useEffect(() => {
-    form.reset({ searchQuery });
-  }, [form, searchQuery]);
-
+  
   
 
   return (
